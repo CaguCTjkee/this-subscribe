f4a73864f8bfc812421ca0a1d20745667a6ce878
CaguCTjkee <caguct.mac@yandex.ru>
2017-12-01 10:07:26 +0200

    add absctact model, subscribers api, etc.

--- wp-content/plugins/this-subscribe/this-subscribe.php
+++ wp-content/plugins/this-subscribe/this-subscribe.php
@@ -19,24 +19,27 @@ if ( defined( 'PL_URL' ) === false ) {
 if ( defined( 'DS' ) === false ) {
 	define( 'DS', DIRECTORY_SEPARATOR );
 }
+if ( defined( 'PL_TEMPLATES' ) === false ) {
+	define( 'PL_TEMPLATES', PL_ROOT . DS . 'templates' );
+}
 
 include_once PL_ROOT . DS . 'autoload.php';
 
 // Register short code
-add_shortcode( 'thisSubscribe', array( 'ThisSubscribe\Init', 'shortCode' ) );
+add_shortcode( 'thisSubscribe', array( 'ThisSubscribe\Controller', 'shortCode' ) );
 
 // Register send mail when post insert
-add_action( 'wp_insert_post', array( 'ThisSubscribe\Init', 'insertPost' ), 10, 3 );
+add_action( 'wp_insert_post', array( 'ThisSubscribe\Controller', 'insertPost' ), 10, 3 );
 
 // Register plugin scripts
-add_action( 'wp_enqueue_scripts', array( 'ThisSubscribe\Init', 'addScripts' ) );
+add_action( 'wp_enqueue_scripts', array( 'ThisSubscribe\Controller', 'addScripts' ) );
 
 // Register ajax event for subscribe
-add_action( 'wp_ajax_add_mail', array( 'ThisSubscribe\Init', 'addMail' ) );
-add_action( 'wp_ajax_nopriv_add_mail', array( 'ThisSubscribe\Init', 'addMail' ) );
+add_action( 'wp_ajax_add_mail', array( 'ThisSubscribe\Controller', 'addMail' ) );
+add_action( 'wp_ajax_nopriv_add_mail', array( 'ThisSubscribe\Controller', 'addMail' ) );
 
 // Add admin menu
-add_action( 'admin_menu', array( 'ThisSubscribe\Init', 'pluginMenu' ) );
+add_action( 'admin_menu', array( 'ThisSubscribe\Controller', 'pluginMenu' ) );
 
 // Add "ts_mails" table to WP when plugin activating
-register_activation_hook( __FILE__, array( 'ThisSubscribe\Init', 'install' ) );
+register_activation_hook( __FILE__, array( 'ThisSubscribe\Controller', 'install' ) );
